6da21ca0a21b0a488ee7c43628e923f005876552
fscbest <romangeyber@gmail.com>
2016-05-25 15:17:44 +0300

    Add .gitignore

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ app.all('/*', function(req, res){
 });
 
 app.listen(PORT, function(){
-    console.log('Server running on ' + PORT);
+    console.log('Server is running on ' + PORT + 'port');
 });
